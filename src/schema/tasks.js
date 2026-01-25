381616d84b259f68c58395cba645fd59907b0738
Adhityae-506 <adhityaelangovan2006@gmail.com>
2026-01-25 12:13:58 +0530

    backend initial commit

---
+++ src/schema/tasks.js
@@ -0,0 +1,11 @@
+import {pgTable,uuid,text,boolean,date,timestamp} from "drizzle-orm/pg-core";
+import {users} from "./users";
+
+export const tasks = pgTable("tasks", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: uuid("user_id").references(() => users.id),
+  title: text("title").notNull(),
+  completed: boolean("completed").default(false),
+  taskDate: date("task_Date").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+});
