8e993fb49c91647462449b8ee2845b9959bc42f8
Adhityae-506 <adhityaelangovan2006@gmail.com>
2026-01-29 06:14:59 +0530

    chnages in db

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,3 @@
-// import "dotenv/config"
-// import { drizzle } from "drizzle-orm/postgres-js"
-// import postgres from "postgres"
-
-// let connectionString = process.env.DATABASE_URL
-// if (host.includes("postgres:postgres@supabase_db_")) {
-//   const url = URL.parse(host)
-//   url.hostname = url.hostname.split("_")[1]
-//   connectionString = url.href
-// }
-
-// export const client = postgres(connectionString, { prepare: false })
-// export const db = drizzle(client)
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
