cfd7dc0d1c8381f57f356dd455df8f9daf7c75c2
Adhityae-506 <adhityaelangovan2006@gmail.com>
2026-02-03 23:38:58 +0530

    Revert "reset Page UI added"
    This reverts commit e3075843076b79d36c92db8cff0c728fe26a23c4.
    
    e

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
 import ForgotPassword from "../pages/auth/ForgotPassword";
-import ResetPassword from "../pages/auth/ResetPassword";
 import Dashboard from "../pages/dashboard/Dashboard";
 
 const AppRoutes = () => {
@@ -14,7 +13,6 @@ const AppRoutes = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password/:token" element={<ResetPassword />} />
                 <Route path="/dashboard" element={<Dashboard/>}/>   
             </Routes>
         </BrowserRouter>
